284cbc63e9c82b73c156fcda4ac38de7c2d04188
syazwan <syazwan9889@gmail.com>
2020-01-26 18:24:31 +0800

    made changes

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+###########################
+cd C:\Users\SZWNPC\OneDrive\_DataScience\002_wuhan_tracker\news_json
+# switch to branch you want to use
+git checkout master
+# add all added/modified files
+git add .
+# commit changes
+git commit -am "made changes"
+# push to git remote repository
+git push
+###########################
+echo Press Enter...
+read
